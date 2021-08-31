4e2349ac26f3ccd2cc817ff06a966707d90bca5b
Sebastian Gustafsson <sebastian.gustafsson@utb.ecutbildning.se>
2021-08-31 15:02:22 +0200

    Fixade lite i "Extrem bonus" den omvända arrayen ska vara {'E', 'D', 'C', 'B', 'A'} och inte {'E', 'D', 'C', 'A', 'B'} Reverse fungerar till vilken array som helst.

--- Tests/ArrayKatas/Kata1.cs
+++ Tests/ArrayKatas/Kata1.cs
@@ -28,7 +28,7 @@ namespace Tests.ArrayKatas
         }
 
         // Extrem bonus - använd templating för att göra så att 'Reverse' fungerar på vilken array som helst
-        /*[Theory]
+        [Theory]
         [InlineData(new[] { 'A', 'B', 'C', 'D', 'E' }, new[] { 'E', 'D', 'C', 'A', 'B' })]
         public void GeneralArrayReverserTest(char[] original, char[] reverse)
         {
@@ -36,6 +36,6 @@ namespace Tests.ArrayKatas
 
             Assert.Equal(reverse, original);
         }
-        */
+        
     }
 }
